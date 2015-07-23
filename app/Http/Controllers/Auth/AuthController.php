9c6ab1060d38453c3529fe590a25f4c662566a1c
Marcelo <marceloctj@gmail.com>
2015-07-22 22:15:28 -0300

    Correção de Variavel de Estados

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -63,16 +63,4 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
-
-    public function estados()
-    {
-        return [ 
-        ''  =>'Selecione',           'AC'=>'Acre',              'AL'=>'Alagoas',   'AP'=>'Amapá',
-        'AM'=>'Amazonas',            'BA'=>'Bahia',             'CE'=>'Ceará',     'DF'=>'Distrito Federal',
-        'ES'=>'Espírito Santo',      'GO'=>'Goiás',             'MA'=>'Maranhão',  'MT'=>'Mato Grosso',
-        'MS'=>'Mato Grosso do Sul',  'MG'=>'Minas Gerais',      'PA'=>'Pará',      'PB'=>'Paraíba',
-        'PR'=>'Parána',              'PE'=>'Pernambuco',        'PI'=>'Piauí',     'RJ'=>'Rio de Janeiro',
-        'RN'=>'Rio Grande do Norte', 'RS'=>'Rio Grande do Sul', 'RO'=>'Rondônia',  'RR'=>'Roraima',
-        'SC'=>'Santa Catarina',      'SE'=>'Sergipe',           'SP'=>'São Paulo', 'TO'=>'Tocantins'];
-    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->share('estados', [ 
+        ''  =>'Selecione',           'AC'=>'Acre',              'AL'=>'Alagoas',   'AP'=>'Amapá',
+        'AM'=>'Amazonas',            'BA'=>'Bahia',             'CE'=>'Ceará',     'DF'=>'Distrito Federal',
+        'ES'=>'Espírito Santo',      'GO'=>'Goiás',             'MA'=>'Maranhão',  'MT'=>'Mato Grosso',
+        'MS'=>'Mato Grosso do Sul',  'MG'=>'Minas Gerais',      'PA'=>'Pará',      'PB'=>'Paraíba',
+        'PR'=>'Parána',              'PE'=>'Pernambuco',        'PI'=>'Piauí',     'RJ'=>'Rio de Janeiro',
+        'RN'=>'Rio Grande do Norte', 'RS'=>'Rio Grande do Sul', 'RO'=>'Rondônia',  'RR'=>'Roraima',
+        'SC'=>'Santa Catarina',      'SE'=>'Sergipe',           'SP'=>'São Paulo', 'TO'=>'Tocantins']);
     }
 
     /**
